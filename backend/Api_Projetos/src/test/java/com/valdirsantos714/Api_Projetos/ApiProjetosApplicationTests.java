da7faf5caa2f408b9fc0d8cf50b41371481f95bc
Valdir Santos <valdirsantost40@gmail.com>
2024-08-15 16:36:58 -0300

    Backend: Vinculado repositório ao banco de dados

---
+++ backend/Api_Projetos/src/test/java/com/valdirsantos714/Api_Projetos/ApiProjetosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valdirsantos714.Api_Projetos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProjetosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
